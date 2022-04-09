6b3a800d6093daa3bc37b9cb30a4ca428fb74de3
Oscar Delicaat <oscar.delicaat.osv@fedex.com>
2022-04-09 23:40:01 +0200

    Added mac

--- src/ssh/protocol.py
+++ src/ssh/protocol.py
@@ -1,3 +1,4 @@
+import hmac
 import secrets
 import socket
 
@@ -6,9 +7,15 @@ class SshTLPProtocol:
     #  https://datatracker.ietf.org/doc/html/rfc4253#page-4
     packet_length_header = 4
     padding_length_header = 1
+    min_padding_size = 4
+    encryption_algorithm = 'aes128-cbc'
+    hash_algorithm = 'sha1-96'
 
-    def __init__(self, cipher_block_length):
+    def __init__(self, cipher_block_length, mac_length):
         self.package_multiple = max(cipher_block_length, 8)
+        self.min_packet_size = max(cipher_block_length, 16)
+        self.mac_length = mac_length
+        self.key = None
 
     def _recv_size(self, socket: socket.socket, msg_size: int) -> bytes:
         contents = b''
@@ -27,24 +34,50 @@ class SshTLPProtocol:
         payload = self._recv_size(socket, payload_size)
         return payload
 
-    def send_message(self, socket: socket.socket, payload: bytes):
+    def send_message(self, socket: socket.socket, payload: bytes, sequence_number: int, encrypted=True):
+        # todo: if encryption enabled use it
         msg_size = len(payload)
         try:
             packet_length = msg_size.to_bytes(self.packet_length_header, 'little', signed=False)
         except OverflowError:
             raise ValueError(f'The message is too long. Max size is {2**self.packet_length_header}')
 
-        # 4 < padding length <255
+        # 4 < padding length < 255
         padding_size = self.packet_length_header + self.padding_length_header + len(payload) % self.package_multiple
+        # bytes to add because of minimum package size
+        bytes_to_add = max(0, self.min_packet_size - (self.packet_length_header + self.padding_length_header + len(payload)) - padding_size)
+        padding_size += bytes_to_add
         if padding_size == 0:
             padding_size = self.package_multiple
-        elif padding_size <= 4:
+        elif padding_size <= self.min_padding_size:
             padding_size = self.package_multiple - padding_size + 1
 
-        msg = packet_length
-        msg += padding_size.to_bytes(self.padding_length_header, 'little', signed=False)
-        msg += payload
-        msg += secrets.token_bytes(padding_size)
+        unencrypted_packet = packet_length
+        unencrypted_packet += padding_size.to_bytes(self.padding_length_header, 'little', signed=False)
+        unencrypted_packet += payload
+        unencrypted_packet += secrets.token_bytes(padding_size)
+        if encrypted:
+            encrypted_packet = self.encrypt(unencrypted_packet)
+            mac = self.mac(sequence_number, unencrypted_packet)
+            msg = encrypted_packet + mac
+        else:
+            msg = unencrypted_packet
 
-        assert len(msg) % self.package_multiple == 0
         socket.send(msg)
+
+    def mac(self, sequence_number: int, unencrypted_packet: bytes) -> bytes:
+        if self.key is None:
+            raise ValueError("Key not set.")
+
+        sequence_number_bytes = sequence_number.to_bytes(4, 'little', signed=False)
+        return hmac.new(
+            key=self.key,
+            msg=sequence_number_bytes + unencrypted_packet,
+            digestmod=self.hash_algorithm
+        ).digest()
+
+    def encrypt(self, packet):
+        return packet
+
+    def set_key(self, key: bytes):
+        self.key = key
