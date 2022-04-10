fa5e010d5c26a75cbbe7032b7684f3c10ca6aee1
Oscar Delicaat <oscar.delicaat.osv@fedex.com>
2022-04-10 19:30:18 +0200

    add dh + test

--- src/ssh/key_exchange.py
+++ src/ssh/key_exchange.py
@@ -1,9 +1,57 @@
+import hashlib
+import random
 import secrets
 from dataclasses import dataclass
+from enum import Enum
+
+
+class MessageCodes(Enum):
+    SSH_MSG_KEXINIT = 20
+    SSH_MSG_KEXDH_INIT = 200
+    SSH_MSG_KEXDH_REPLY = 201
 
 
 @dataclass
-class StartKeyExchangeMessage:
+class Message:
+    @staticmethod
+    def to_two_complement_bytestring(value: int) -> bytes:
+        two_complement = '0' + format(value, 'b')
+        return KeyExchangeDHReply.to_bytestring(two_complement)
+
+    @staticmethod
+    def from_two_complement_bytestring(index: int, payload: bytes) -> (int, int):
+        index, binary_value_string = KeyExchangeDHReply.from_bytestring(index, payload)
+        return index, int(binary_value_string, 2)
+
+    @staticmethod
+    def to_bytestring(value: str) -> bytes:
+        name_list_size_bytes = len(value.encode('utf-8')).to_bytes(4, 'little', signed=False)
+        return name_list_size_bytes + value.encode()
+
+    @staticmethod
+    def from_bytestring(index: int, payload: bytes) -> (int, str):
+        length = int.from_bytes(payload[index:index + 4], 'little', signed=False)
+        return index + 4 + length, payload[index + 4:index + 4 + length].decode()
+
+    @staticmethod
+    def from_name_list(index, payload) -> (int, str):
+        """ format of nameslist: `("<comma separated list of names>"),`
+            but simplify to `(<name>),`
+        """
+        length = int.from_bytes(payload[index:index + 4], 'little', signed=False)
+        name_list_start_index = index + 4 + 1
+        index += 4 + 1 + length + 2
+        return index, payload[name_list_start_index: name_list_start_index + length].decode('utf-8')
+
+    @staticmethod
+    def to_name_list(names: str) -> bytes:
+        name_list = f"({names}),"
+        name_list_size_bytes = len(names.encode('utf-8')).to_bytes(4, 'little', signed=False)
+        return name_list_size_bytes + name_list.encode()
+
+
+@dataclass
+class StartKeyExchange(Message):
     """
     To be send by on connection established on server and first message send by client.
     (Actually first msg should be protocol version exchange but not implementing that)
@@ -18,20 +66,20 @@ class StartKeyExchangeMessage:
     first_kex_packet_follows: bool
 
     def __bytes__(self):
-        SSH_MSG_KEXINIT = 20
+        msg_code = MessageCodes.SSH_MSG_KEXINIT.value
         # name lists according to https://datatracker.ietf.org/doc/html/rfc4251
         # Set list of key exchange algo's
         cookie = secrets.token_bytes(16)
-        kex_algorithms = self.name_list(self.kex_algorithms)
-        server_host_key_algorithms = self.name_list(self.server_host_key_algorithms)
-        encryption_algorithms_client_to_server = self.name_list(self.encryption_algorithms_client_to_server)
-        encryption_algorithms_server_to_client = self.name_list(self.encryption_algorithms_server_to_client)
-        mac_algorithms_client_to_server = self.name_list(self.mac_algorithms_client_to_server)
-        mac_algorithms_server_to_client = self.name_list(self.mac_algorithms_server_to_client)
+        kex_algorithms = self.to_name_list(self.kex_algorithms)
+        server_host_key_algorithms = self.to_name_list(self.server_host_key_algorithms)
+        encryption_algorithms_client_to_server = self.to_name_list(self.encryption_algorithms_client_to_server)
+        encryption_algorithms_server_to_client = self.to_name_list(self.encryption_algorithms_server_to_client)
+        mac_algorithms_client_to_server = self.to_name_list(self.mac_algorithms_client_to_server)
+        mac_algorithms_server_to_client = self.to_name_list(self.mac_algorithms_server_to_client)
         # If first_kex_packet_follows is False since not guessing initial kex so no msg following
         first_kex_packet_follows = bytes(int(self.first_kex_packet_follows))
 
-        msg = SSH_MSG_KEXINIT.to_bytes(1, 'little', signed=False)
+        msg = msg_code.to_bytes(1, 'little', signed=False)
         msg += cookie
         msg += kex_algorithms
         msg += server_host_key_algorithms
@@ -46,12 +94,12 @@ class StartKeyExchangeMessage:
     @classmethod
     def from_bytes(cls, payload: bytes):
         index = 16 + 1
-        index, kex_algorithms = cls.parse_name_list(index, payload)
-        index, server_host_key_algorithms = cls.parse_name_list(index, payload)
-        index, encryption_algorithms_client_to_server = cls.parse_name_list(index, payload)
-        index, encryption_algorithms_server_to_client = cls.parse_name_list(index, payload)
-        index, mac_algorithms_client_to_server = cls.parse_name_list(index, payload)
-        index, mac_algorithms_server_to_client = cls.parse_name_list(index, payload)
+        index, kex_algorithms = cls.from_name_list(index, payload)
+        index, server_host_key_algorithms = cls.from_name_list(index, payload)
+        index, encryption_algorithms_client_to_server = cls.from_name_list(index, payload)
+        index, encryption_algorithms_server_to_client = cls.from_name_list(index, payload)
+        index, mac_algorithms_client_to_server = cls.from_name_list(index, payload)
+        index, mac_algorithms_server_to_client = cls.from_name_list(index, payload)
         first_kex_packet_follows = bool(int.from_bytes(payload[index: index + 1], 'little', signed=False))
         return cls(
             kex_algorithms=kex_algorithms, server_host_key_algorithms=server_host_key_algorithms,
@@ -62,33 +110,69 @@ class StartKeyExchangeMessage:
             first_kex_packet_follows=first_kex_packet_follows
         )
 
-    @staticmethod
-    def parse_name_list(index, payload) -> (int, str):
-        """ format of nameslist: `("<comma separated list of names>"),`
-            but simplify to `(<name>),`
-        """
-        length = int.from_bytes(payload[index:index + 4], 'little', signed=False)
-        name_list_start_index = index + 4 + 1
-        index += 4 + 1 + length + 2
-        return index, payload[name_list_start_index: name_list_start_index + length].decode('utf-8')
 
-    def name_list(self, names: str) -> bytes:
-        name_list = f"({names}),"
-        name_list_size_bytes = len(names.encode('utf-8')).to_bytes(4, 'little', signed=False)
-        return name_list_size_bytes + name_list.encode()
+@dataclass
+class StartKeyExchangeDH(Message):
+    """ Used to start DH kex """
+    e: int
+
+    def __bytes__(self):
+        msg_code = MessageCodes.SSH_MSG_KEXDH_INIT.value
+        msg = msg_code.to_bytes(1, 'little', signed=False)
+        two_complement_e = '0' + format(self.e, 'b')
+        msg += two_complement_e.encode('utf-8')
+        return msg
+
+    @classmethod
+    def from_bytes(cls, payload: bytes):
+        binary_value = payload[2:].decode()
+        e = int(binary_value, 2)
+        return cls(e=e)
+
+@dataclass
+class KeyExchangeDHReply(Message):
+    """ Used to start DH kex """
+    server_host_key_certificates: str
+    f: int
+    signature_h: str
+
+    def __bytes__(self):
+        msg_code = MessageCodes.SSH_MSG_KEXDH_REPLY.value
+        msg = msg_code.to_bytes(1, 'little', signed=False)
+        msg += self.to_bytestring(self.server_host_key_certificates)
+        msg += self.to_two_complement_bytestring(self.f)
+        msg += self.to_bytestring(self.signature_h)
+        return msg
+
+    @classmethod
+    def from_bytes(cls, payload: bytes):
+        index = 1
+        index, server_host_key_certificates = cls.from_bytestring(index, payload)
+        index, f = cls.from_two_complement_bytestring(index, payload)
+        index, signature_h = cls.from_bytestring(index, payload)
+        return cls(
+            server_host_key_certificates=server_host_key_certificates, f=f,
+            signature_h=signature_h
+        )
 
 
-msg = StartKeyExchangeMessage(
-    kex_algorithms='diffie-hellman-group1-sha1',
-    server_host_key_algorithms='ssh-ecdsa',
-    encryption_algorithms_client_to_server='aes128-cbc',
-    encryption_algorithms_server_to_client='aes128-cbc',
-    mac_algorithms_client_to_server='sha1-96',
-    mac_algorithms_server_to_client='sha1-96',
-    first_kex_packet_follows=False,
-)
+class DiffieHellmanKeyExchange:
+    # Todo: Replace with proper Oakley gp 2 params.
+    p = 23
+    g = 5
+    q = 100000
 
-byte_msg = bytes(msg)
+    def __init__(self):
+        self.x = random.randint(1, self.q)
 
+    def generate_partial_shared_key(self):
+        return (self.g ** self.x) % self.p
 
+    def calculate_shared_key(self, received_partial_shared_key):
+        return (received_partial_shared_key ** self.x) % self.p
+
+    def calculate_hash(self, v_c: str, v_s: str, i_c: str, i_s: str, k_s: str, e: str, f: str, k: str) -> str:
+        # Todo: Replace with proper sha-1 hash
+        to_hash = v_c + v_s + i_c + i_s + k_s + e + f + k
+        return hashlib.sha1(to_hash).hexdigest()
 
--- tests/test_key_exchange.py
+++ tests/test_key_exchange.py
@@ -1,8 +1,9 @@
-from ssh.key_exchange import StartKeyExchangeMessage
+from ssh.key_exchange import StartKeyExchange, StartKeyExchangeDH, KeyExchangeDHReply, \
+    DiffieHellmanKeyExchange
 
 
 def test_start_kex_msg():
-    msg = StartKeyExchangeMessage(
+    msg = StartKeyExchange(
         kex_algorithms='diffie-hellman-group1-sha1',
         server_host_key_algorithms='ssh-ecdsa',
         encryption_algorithms_client_to_server='aes128-cbc',
@@ -12,6 +13,31 @@ def test_start_kex_msg():
         first_kex_packet_follows=False,
     )
 
-    msg_from_bytes = StartKeyExchangeMessage.from_bytes(bytes(msg))
+    msg_from_bytes = StartKeyExchange.from_bytes(bytes(msg))
 
     assert msg == msg_from_bytes
+
+
+def test_start_kex_dh_msg():
+    msg = StartKeyExchangeDH(1255521414)
+    msg_from_bytes = StartKeyExchangeDH.from_bytes(bytes(msg))
+    assert msg == msg_from_bytes
+
+
+def test_kex_dh_reply_msg():
+    msg = KeyExchangeDHReply(
+        server_host_key_certificates="TEST",
+        f=12412436346,
+        signature_h="sometestsignature"
+    )
+    msg_from_bytes = KeyExchangeDHReply.from_bytes(bytes(msg))
+    assert msg == msg_from_bytes
+
+
+def test_kex_dh():
+    client = DiffieHellmanKeyExchange()
+    client_partial = client.generate_partial_shared_key()
+    server = DiffieHellmanKeyExchange()
+    server_partial = server.generate_partial_shared_key()
+
+    assert client.calculate_shared_key(server_partial) == server.calculate_shared_key(client_partial)
